8d985f68d3836141d179fb8a6e97c2ae43f53f7f
Keyon19 <keyon.gray@mail.citytech.cuny.edu>
2022-04-28 20:57:43 -0400

    4/28/2022, still need to update the code to add the hop and the sound

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -8,17 +8,52 @@ public class playermovement : MonoBehaviour
 {
 
     public float speed = 5.0f;
-   
-    void Update()
-    {
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float xMovement = xMove * Time.deltaTime;
- 
+    public gamemanager gameManager;
 
-        float yMove = Input.GetAxisRaw("Vertical");
-        float yMovement = yMove * Time.deltaTime;
-        
-        transform.Translate(xMovement, yMovement, 0);
+    public float moveDuration = 0.5f;
+    private float timeElapsed;
+    private Vector3 targetPosition;
+    private bool readyToMove = true;
+    private bool isMoving = false;
+   
+    public enum MovementType
+    {
+        Continuous,
+        Discrete,
+    }
+
+    public MovementType movementType;
+
+
+    private void Start()
+    {
+        targetPosition = transform.position;
+    }
+     
+    void Update() { 
+    if (movementType == MovementType.Continuous)
+        {
+            ContinuousMovement();
+        }
+    else
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                //DiscreteMovement(transform.position, transform.position + Vector3.up);
+            }
+
+            if (targetPosition != transform.position)
+            {
+                DiscreteMovement(transform.position, targetPosition);
+            }
+            //DiscreteMovement();
+        }
+    
+       
+    
+    
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,6 +63,38 @@ public class playermovement : MonoBehaviour
             Destroy(gameObject);
             SceneManager.LoadScene("v1");
         }
+    }
+
+    private void SetTargetPosition()
+    {
+        targetPosition = transform.position + (Vector3.up * .5f);
+    }
+
+    private void ContinuousMovement()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+        
+        float xMovement = xMove * Time.deltaTime;
+        float yMovement = yMove * Time.deltaTime;
+
+        transform.Translate(xMovement, yMovement, 0);
+    }
+
+    private void DiscreteMovement(Vector3 start, Vector3 end)
+    {
+        if(timeElapsed < moveDuration)
+        {
+            timeElapsed += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(start, end, timeElapsed / moveDuration);
+
+        }
+        else
+        {
+            transform.position = targetPosition;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
